fed90383b06d8ecc5677fb7b4d51788d74c235ee
wwojtas <wojciech.wus.programista@gmail.com>
2022-03-22 21:40:04 +0100

    getting attachments

--- src/com/email/model/EmailMessage.java
+++ src/com/email/model/EmailMessage.java
@@ -5,7 +5,11 @@ import javafx.beans.property.SimpleObjectProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeBodyPart;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class EmailMessage {
 
@@ -16,6 +20,9 @@ public class EmailMessage {
     private SimpleObjectProperty<Date> date;
     private boolean isRead;
     private Message message;
+    List<MimeBodyPart> attachmentList = new ArrayList<MimeBodyPart>();
+    private boolean hasAttachments = false;
+
 
     public EmailMessage(String subject, String sender, String recipient, int size, Date date, boolean isRead, Message message){
         this.subject = new SimpleStringProperty(subject);
@@ -53,4 +60,14 @@ public class EmailMessage {
     }
 
 
+    public void addAttachment(MimeBodyPart mbp) {
+        hasAttachments = true;
+        attachmentList.add(mbp);
+        try {
+            System.out.println("Added attach: " + mbp.getFileName());
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
